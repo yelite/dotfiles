e36ffa0c7903f514f8d85c7bb27034766baf1f54
Lite Ye <liteye859@gmail.com>
2019-07-01 21:47:34 -0400

    Add installation script

---
+++ install.sh
@@ -0,0 +1,18 @@
+function install_dot_file {
+    readonly src=${1:?}
+    readonly dst=$HOME/${2:?}
+
+    if [ -f $dst ]
+    then
+        echo "$dst already exists"
+    else
+        echo "$src installed to $dst"
+        ln -s "$HOME/.dotfiles/$src" "$dst"
+    fi
+}
+
+install_dot_file "vim/vimrc" ".vimrc"
+install_dot_file "vim/vimrc" ".config/nvim/init.vim"
+install_dot_file "zsh/zshrc" ".zshrc"
+install_dot_file "ideavim/ideavimrc" ".ideavimrc"
+
