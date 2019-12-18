b3e0d9527f27b83a7f1cdcdde0e37f7186696086
Lite Ye <liteye859@gmail.com>
2019-12-18 00:20:19 -0500

    Add doom emacs

--- install.zsh
+++ install.zsh
@@ -15,5 +15,5 @@ install_dot_file "vim/vimrc" ".vimrc"
 install_dot_file "vim/vimrc" ".config/nvim/init.vim"
 install_dot_file "zsh/zshrc" ".zshrc"
 install_dot_file "ideavim/ideavimrc" ".ideavimrc"
-install_dot_file "spacemacs.d" ".spacemacs.d"
+install_dot_file "doom.d" ".doom.d"
 
